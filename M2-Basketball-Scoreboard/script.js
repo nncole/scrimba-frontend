d571dfc191d510655aaf009983d7204ee316c7c6
Nicole Sanchez <sancheznncole@gmail.com>
2025-08-25 09:01:06 +0800

    feat(js): add point system

---
+++ M2-Basketball-Scoreboard/script.js
@@ -0,0 +1,15 @@
+const homeScore = document.getElementById("home-score");
+const guestScore = document.getElementById("guest-score");
+
+let homePoints = 0;
+let guestPoints = 0;
+
+function updateScore(team, points) {
+  if (team === "home") {
+    homePoints += points;
+    homeScore.textContent = homePoints;
+  } else if (team === "guest") {
+    guestPoints += points;
+    guestScore.textContent = guestPoints;
+  }
+}
