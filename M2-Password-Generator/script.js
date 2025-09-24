c548a4c8b842a6043d5aeb30af032a5e06e3bfee
Nicole Sanchez <sancheznncole@gmail.com>
2025-09-24 14:24:51 +0800

    feat(js): add password length feature

--- M2-Password-Generator/script.js
+++ M2-Password-Generator/script.js
@@ -1,24 +1,32 @@
+const passwordLength = document.querySelector("#password-length");
+const sliderValue = document.querySelector("#slider-value");
 const generateBtn = document.querySelector("#generate-btn");
 const passwordsContainer = document.querySelector("#generated-passwords");
 const generatedPassword = document.querySelectorAll(".password")
 
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
-function generatePassword() {
+passwordLength.addEventListener("input", function(){
+  sliderValue.textContent = passwordLength.value;
+})
+
+
+function generatePassword(length) {
   let option = "";
 
-  for (let i = 0; i <= 14; i++){
+  for (let i = 0; i < length; i++){
     const char = Math.floor(Math.random() * characters.length);
     option += characters[char]
   }
-
   return option;
 }
 
 generateBtn.addEventListener("click", function(){
   passwordsContainer.style.visibility = "visible";
 
+  const length = parseInt(passwordLength.value, 10);
+
   for (const password of generatedPassword) {
-    password.textContent = generatePassword();
+    password.textContent = generatePassword(length);
   }
 })
