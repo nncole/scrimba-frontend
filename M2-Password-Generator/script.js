b1f6a8dbecd3af77a9a500617925e75a016e9a7d
Nicole Sanchez <sancheznncole@gmail.com>
2025-09-24 15:24:31 +0800

    feat(js): add toggle for numbers

--- M2-Password-Generator/script.js
+++ M2-Password-Generator/script.js
@@ -1,10 +1,24 @@
 const passwordLength = document.querySelector("#password-length");
 const sliderValue = document.querySelector("#slider-value");
+const includeNumbers = document.querySelector("#include-numbers");
 const generateBtn = document.querySelector("#generate-btn");
 const passwordsContainer = document.querySelector("#generated-passwords");
 const generatedPassword = document.querySelectorAll(".password")
 
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+
+function getCharacters() {
+  let availableCharacters = [...letters];
+
+  if (includeNumbers.checked) {
+    availableCharacters.push(...numbers)
+  }
+  return availableCharacters;
+}
 
 passwordLength.addEventListener("input", function(){
   sliderValue.textContent = passwordLength.value;
@@ -13,6 +27,7 @@ passwordLength.addEventListener("input", function(){
 
 function generatePassword(length) {
   let option = "";
+  const characters = getCharacters();
 
   for (let i = 0; i < length; i++){
     const char = Math.floor(Math.random() * characters.length);
