4183d9a05a71593ee24379ffc2a30a6c59d3887d
Nicole Sanchez <sancheznncole@gmail.com>
2025-10-13 14:47:54 +0800

    feat(js): add conversion feature

---
+++ M2-Unit-Converter/script.js
@@ -0,0 +1,28 @@
+const resLength = document.querySelector("#result-length");
+const resVolume = document.querySelector("#result-volume");
+const resMass = document.querySelector("#result-mass");
+
+const convertBtn = document.querySelector("#convert-btn");
+
+function getConversion() {
+  let userInput = document.querySelector("#user-input").value;
+
+  // Length (Meter/Feet)
+  let feet = userInput * 3.281;
+  let meter = userInput / 3.281;
+  resLength.textContent = `${userInput} meters = ${feet} feet | ${userInput} feet = ${meter} meters`;
+
+  // Volume (Liters/Gallons)
+  let gallon = userInput / 3.785;
+  let liter = userInput * 3.785;
+  resVolume.textContent = `${userInput} liters = ${gallon} gallons | ${userInput} gallons = ${liter} liters`;
+
+  // Mass (Kilograms/Pounds)
+  let pound = userInput * 2.205;
+  let kilogram = userInput / 2.205;
+  resMass.textContent = `${userInput} kilos = ${pound} pounds | ${userInput} pounds = ${kilogram} kilos`;
+};
+
+convertBtn.addEventListener("click", () => {
+  getConversion();
+});
