abfe89fe4e94b3796a11e2f72ff418ae33a9f685
Nicole Sanchez <sancheznncole@gmail.com>
2025-10-13 14:51:02 +0800

    feat: limit decimals

--- M2-Unit-Converter/script.js
+++ M2-Unit-Converter/script.js
@@ -8,18 +8,18 @@ function getConversion() {
   let userInput = document.querySelector("#user-input").value;
 
   // Length (Meter/Feet)
-  let feet = userInput * 3.281;
-  let meter = userInput / 3.281;
+  let feet = (userInput * 3.281).toFixed(3);
+  let meter = (userInput / 3.281).toFixed(3);
   resLength.textContent = `${userInput} meters = ${feet} feet | ${userInput} feet = ${meter} meters`;
 
   // Volume (Liters/Gallons)
-  let gallon = userInput / 3.785;
-  let liter = userInput * 3.785;
+  let gallon = (userInput / 3.785).toFixed(3);
+  let liter = (userInput * 3.785).toFixed(3);
   resVolume.textContent = `${userInput} liters = ${gallon} gallons | ${userInput} gallons = ${liter} liters`;
 
   // Mass (Kilograms/Pounds)
-  let pound = userInput * 2.205;
-  let kilogram = userInput / 2.205;
+  let pound = (userInput * 2.205).toFixed(3);
+  let kilogram = (userInput / 2.205).toFixed(3);
   resMass.textContent = `${userInput} kilos = ${pound} pounds | ${userInput} pounds = ${kilogram} kilos`;
 };
 
