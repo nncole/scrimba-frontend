b045bb7ae59a4eade96a22da887143ec906295bd
Nicole Sanchez <sancheznncole@gmail.com>
2025-10-14 10:24:34 +0800

    fix: require min value

--- M2-Unit-Converter/script.js
+++ M2-Unit-Converter/script.js
@@ -2,27 +2,33 @@ const resLength = document.querySelector("#result-length");
 const resVolume = document.querySelector("#result-volume");
 const resMass = document.querySelector("#result-mass");
 
+const userInput = document.querySelector("#user-input");
 const convertBtn = document.querySelector("#convert-btn");
 
 function getConversion() {
-  let userInput = document.querySelector("#user-input").value;
+  let val = userInput.value;
 
   // Length (Meter/Feet)
-  let feet = (userInput * 3.281).toFixed(3);
-  let meter = (userInput / 3.281).toFixed(3);
-  resLength.textContent = `${userInput} meters = ${feet} feet | ${userInput} feet = ${meter} meters`;
+  let feet = (val * 3.281).toFixed(3);
+  let meter = (val / 3.281).toFixed(3);
+  resLength.textContent = `${val} meters = ${feet} feet | ${val} feet = ${meter} meters`;
 
   // Volume (Liters/Gallons)
-  let gallon = (userInput / 3.785).toFixed(3);
-  let liter = (userInput * 3.785).toFixed(3);
-  resVolume.textContent = `${userInput} liters = ${gallon} gallons | ${userInput} gallons = ${liter} liters`;
+  let gallon = (val / 3.785).toFixed(3);
+  let liter = (val * 3.785).toFixed(3);
+  resVolume.textContent = `${val} liters = ${gallon} gallons | ${val} gallons = ${liter} liters`;
 
   // Mass (Kilograms/Pounds)
-  let pound = (userInput * 2.205).toFixed(3);
-  let kilogram = (userInput / 2.205).toFixed(3);
-  resMass.textContent = `${userInput} kilos = ${pound} pounds | ${userInput} pounds = ${kilogram} kilos`;
+  let pound = (val * 2.205).toFixed(3);
+  let kilogram = (val / 2.205).toFixed(3);
+  resMass.textContent = `${val} kilos = ${pound} pounds | ${val} pounds = ${kilogram} kilos`;
 };
 
 convertBtn.addEventListener("click", () => {
+  if(userInput.value < 1 || userInput.value === "") {
+    alert("Please enter a value greater than or equal to 1.");
+    return;
+  }
+  
   getConversion();
 });
