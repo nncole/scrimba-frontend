e862f1932292e24dc0116ade287dab5cab99cb5a
Nicole Sanchez <sancheznncole@gmail.com>
2025-10-24 14:42:44 +0800

    feat(js): render added menu items

--- M4-Restaurant-Ordering-App/script.js
+++ M4-Restaurant-Ordering-App/script.js
@@ -1,6 +1,9 @@
 import { menuArray } from "./data.js";
 
 const menuContainer = document.querySelector("#menu-container");
+const orderContainer = document.querySelector("#order-container");
+
+// RENDER MENU
 
 function renderMenu() {
   menuContainer.innerHTML = "";
@@ -24,3 +27,47 @@ function renderMenu() {
 };
 
 renderMenu();
+
+// EVENT LISTENER
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("add-btn")) {
+    const itemID = Number(e.target.dataset.id);
+    addOrder(itemID);
+  }
+});
+
+// ADD/REMOVE ORDERS
+
+let orders = [];
+
+function addOrder(itemID) {
+  const item = menuArray.find(item => item.id === itemID);
+  orders.push(item);
+  renderOrder();
+};
+
+// RENDER ORDERS
+
+function renderOrder() {
+  orderContainer.innerHTML = `
+    <h2>Your Order</h2>
+
+    <div class="order-item">
+      ${orders.map(item => `
+        <p class="name">${item.name}</p>
+        <button class="remove-btn" data-id="${item.id}">remove</button>
+        <p class="price">$${item.price}</p>
+      `).join("")}
+    </div>
+
+    <div class="divider"></div>
+
+    <div class="order-total">
+      <h3>Total price:</h3>
+      <div class="price" id="total-price">$0</div>
+    </div>
+
+    <button id="complete-order-btn">Complete order</button>
+  `;
+};
