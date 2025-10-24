9fcd161f5fb183d2e179d1e1e289bf9560c32f1f
Nicole Sanchez <sancheznncole@gmail.com>
2025-10-24 10:14:54 +0800

    feat(js): add menu data

---
+++ M4-Restaurant-Ordering-App/data.js
@@ -0,0 +1,23 @@
+export const menuArray = [
+  {
+    id: 1,
+    name: "Pizza",
+    ingredients: ["Pepperoni", "Mushroom", "Mozarella"],
+    price: 14,
+    emoji: "🍕"
+  },
+  {
+    id: 2,
+    name: "Hamburger",
+    ingredients: ["Beef", "Cheese", "Lettuce"],
+    price: 12,
+    emoji: "🍔",
+  },
+  {
+    id: 3,
+    name: "Beer",
+    ingredients: ["Grain", "Hops", "Yeast", "Water"],
+    price: 12,
+    emoji: "🍺",
+  },
+];
